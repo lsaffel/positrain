81d13d7d9008999d0d06dd3dbe8f72bbd3066207
lsaffel <58794458+lsaffel@users.noreply.github.com>
2020-08-30 09:53:39 -0600

    Add files via upload

---
+++ dogscriptgit.js
@@ -0,0 +1,19 @@
+// JavaScript source code
+
+/*	Lisa Saffel
+    June 1, 2020
+    CPRG 210
+    HTML / CSS / Javascript
+    Day 1 to 7 Daily Exercises
+*/
+
+
+// function to check to be sure that the user wants to submit the form for sure or not - register page
+function confirm_submit() {
+    return confirm("Are you sure you want to submit?");
+}
+
+// function to check to be sure that the user wants to reset the form for sure or not - register page
+function confirm_reset() {
+    return confirm("Are you sure you want to reset?");
+}
