9e22fe83864f488a5d4c8a1857c686842dd7bdfe
lsaffel <llvvss@icloud.com>
2020-09-14 20:45:04 -0600

    Edits and duplicate file deletion
    Some duplicate files removed. Comments edited.

--- dogscriptgit.js
+++ dogscriptgit.js
@@ -1,13 +1,11 @@
 // JavaScript source code
 
-/*	Lisa Saffel
-    June 1, 2020
-    CPRG 210
-    HTML / CSS / Javascript
-    Day 1 to 7 Daily Exercises
+/*	Author: Lisa Saffel
+			Date: June 1, 2020
+			Languages: HTML / CSS / Javascript
+			Purpose: A web site with an index, contact page and register page
 */
 
-// note - most of the javascript seems to be missing from this site as it is right now ****
 
 // function to check to be sure that the user wants to submit the form for sure or not - register page
 function confirm_submit() {
